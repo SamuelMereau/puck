485d94234fe048058dbd284c510435244d496773
Samuel Mereau <samuelmereau@gmail.com>
2022-01-22 21:28:40 +1100

    Serves an SVG to the endpoint for HTML images

--- app.js
+++ app.js
@@ -18,13 +18,19 @@ app.get('/:imgSize/:imgHeight?', async function (req, res) {
         const imgSize = req.params.imgSize;
         const imgHeight = req.params.imgHeight;
         console.log({url, imgSize, imgHeight});
-        let data = "test";
-        request.get(url, function (err, response, body) {
+        let data = "";
+        request({
+            url: url,
+            encoding: null
+        }, 
+        function (err, response, body) {
             if (!err && response.statusCode == 200) {
                 data = 'data:' + response.headers["content-type"] + ';base64,' + Buffer.from(body).toString('base64');
             }
         }).on('complete', () => {
-            res.send(`<img src="${data}" alt="test"/>`);
+            const element = `<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" style=""><image href="${data}" height="50" width="50"/></svg>`
+            res.setHeader('content-type', 'image/svg+xml');
+            res.send(element);
         });
     } catch (e) {
         console.log(`Error: ${e}`);
