6b37172c24374c1348186b42a0a4918489d6b62e
Samuel Mereau <samuelmereau@gmail.com>
2022-01-25 15:13:23 +1100

    Revert "Remove unused parameters in app.js"
    This reverts commit 9605c02ab057ad0a65d53d46a40209a65715783d.

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ import app from './src/startup/routes.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => { console.log(`Server is listening on port ${port}`) });
+app.listen(port, function (req, res) {
+    console.log(`Server is listening on port ${port}`);
+});
