df2af664ee850d34c4eea574f48724a5f1fc5240
Samuel Mereau <53167217+SamuelMereau@users.noreply.github.com>
2022-01-29 23:02:54 +1100

    Delete express-v1 directory

--- express-v1/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import startup from './startup/routes.js';
-const app = express();
-
-startup(app);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => { console.log(`Server is listening on port ${port}`); });
--- express-v1/startup/routes.js
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-
-import imgRouter from '../routes/img.js';
-
-export default function startup(app) {
-    app.use(cors());
-    app.use('/', imgRouter);
-};
